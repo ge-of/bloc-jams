c9cbc57354838a5a042973645eefc9e0bc6be589
Geoff Miles <miles.geoff@gmail.com>
2017-03-01 20:57:23 -0500

    Animate Points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
- var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
- 
+ var animatePoints = function(points) {
      var revealFirstPoint = function() {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
